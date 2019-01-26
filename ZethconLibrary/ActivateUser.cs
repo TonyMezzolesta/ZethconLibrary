a9776b904508ae3102780721b3e6bdf3e3a10776
amezzolesta <amezzolesta@gmail.com>
2019-01-25 23:05:26 -0500

    initial checkin + began building ActiveUser API call.

---
+++ ZethconLibrary/ActivateUser.cs
@@ -0,0 +1,86 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZethconLibrary
+{
+    internal class ActivateUser
+    {
+        internal static ActivateUserResponseModel ActivateUserMethod(ActivateUserRequestModel requestApp)
+        {
+            ActivateUserResponseModel responseModel = new ActivateUserResponseModel();
+
+            try
+            {
+                var client = new RestClient("https://wds.zethconapptest.com/activateuser");
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("cache-control", "no-cache");
+                request.AddHeader("Content-Type", "application/json");
+                request.AddParameter("undefined", requestApp, ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+
+                //convert response to object
+                responseModel = Newtonsoft.Json.JsonConvert.DeserializeObject<ActivateUserResponseModel>(response.Content);
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            return responseModel;
+        }
+
+        
+    }
+
+    #region Structure
+
+    public struct ActivateUserRequestModel
+    {
+        public string AuthToken;
+        public ActivateUserRequest ActivateUserRequest;
+
+        public ActivateUserRequestModel(string authtoken, ActivateUserRequest activateUserRequest)
+        {
+            AuthToken = authtoken;
+            ActivateUserRequest = activateUserRequest;
+        }
+    }
+
+    public struct ActivateUserRequest
+    {
+        public string Nameid;
+
+        public ActivateUserRequest(string nameid)
+        {
+            Nameid = nameid;
+        }
+    }
+
+    public struct ActivateUserResponseModel
+    {
+        public string User;
+
+        public ActivateUserResponseModel(string user)
+        {
+            User = user;
+        }
+    }
+
+    #endregion
+
+    #region Response exception
+
+    public class ActivateUserRequestException : Exception
+    {
+        public ActivateUserRequestException(string message)
+            : base(message)
+        {
+        }
+    }
+
+    #endregion
+}
---
+++ ZethconLibrary/ZethconAPI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZethconLibrary
+{
+    public class ZethconAPI
+    {
+        public static ActivateUserResponseModel ActivateUserAPI(ActivateUserRequestModel requestApp)
+        {
+            try
+            {
+                return ActivateUser.ActivateUserMethod(requestApp);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
